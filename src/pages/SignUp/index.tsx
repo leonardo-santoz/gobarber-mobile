980347a5f4fe242655da4290d079c6f23f54bcdd
Leonardo Santos <leonardo.cienciacomputer@gmail.com>
2020-09-17 21:10:24 -0300

    03 - Cadatro na aplicaçao

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -10,13 +10,12 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
-import * as Yup from 'yup';
-
-import getValidationErrors from '../../utils/getValidationErrors';
-
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import api from '../../services/api';
 
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -60,10 +59,14 @@ const SignUp: React.FC = () => {
           abortEarly: false,
         });
 
-        // await api.post('users', data);
+        await api.post('users', data);
 
-        // history.push('/');
+        Alert.alert(
+          'Cadastro realizado com sucesso!',
+          'Você já pode fazer login na aplicação.app'
+        )
 
+        navigation.goBack();
 
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
@@ -75,7 +78,7 @@ const SignUp: React.FC = () => {
         }
 
         Alert.alert('Erro no cadastro',
-        'Ocorreu um erro ao fazer cadastro.'
+          'Ocorreu um erro ao fazer cadastro.'
         );
       }
     }, []);
